3ab2f37dd7bcd530bf1f57f0768a412ee16af9cf
eshah1028 <eshah1028@gmail.com>
2026-05-05 19:42:05 -0700

    explore part 1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 5 - Starter
 Esha Shah  
-Expose: https://eshah1028.github.io/CSE110_Lab5/expose.html
+Expose: https://eshah1028.github.io/CSE110_Lab5/expose.html  
+1.  No, because the message feature isn't a specific, isolated function but likely incorporate multipled different features that should be individually tested as well
+2.  Yes, because it can be easily validated with a unit test, given it has it's own isolated specification
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,52 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const icon = document.querySelector('img');
+  const text = document.getElementById('text-to-speak');
+  const play = document.querySelector('button');
+
+  let voices = [];
+
+  function getVoiceList(){
+    voices = synth.getVoices();
+
+    voices.forEach((voice, i) => {
+      const option = document.createElement('option');
+      option.value = i;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  getVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = getVoiceList;
+  }
+
+  play.addEventListener('click', () => {
+    const speech = text.value;
+
+    if (!speech) return;
+
+    const utterance = new SpeechSynthesisUtterance(speech);
+    const selectedIndex = voiceSelect.value;
+    if (selectedIndex !== "select") {
+      utterance.voice = voices[selectedIndex];
+    }
+
+    utterance.onstart = () => {
+      icon.src = 'assets/images/smiling-open.png';
+    };
+
+    
+    utterance.onend = () => {
+      icon.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utterance);
+  });
+
 }
